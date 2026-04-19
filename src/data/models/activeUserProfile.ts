547dd3b77906afdf30bd8fc41b5e66bb80d98370
devostafa <mostafa.maher98@gmail.com>
2026-04-19 18:33:32 +0200

    Created basic user model and implemented MenuBar component structure

---
+++ src/data/models/activeUserProfile.ts
@@ -0,0 +1,6 @@
+import type { UserType } from "./userType.ts";
+
+export interface ActiveUserProfile {
+  name: string,
+  type: UserType
+}
---
+++ src/views/components/menuBar.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import type { ActiveUserProfile } from "../../data/models/activeUserProfile";
+
+export default function MenuBar() {
+  const [userProfileCard, setUserProfileCard] = useState<ActiveUserProfile>();
+
+  return <div>
+
+  </div>
+}
